d42bd63db2eb47d727d5aa9a650ed3da739983af
azerum <60925680+azerum@users.noreply.github.com>
2023-02-14 14:23:22 +0100

    add Cmd and Sub

--- out/main.js
+++ out/main.js
@@ -1,21 +1,42 @@
 import { useEffect, useReducer } from 'react';
-const kCommand = Symbol('kCommand');
-export default function useElm(initialState, update, execute, subscribe) {
-    const [internal, message] = useReducer((model, message) => {
-        return toInternal(update(model, message));
-    }, toInternal(initialState));
-    /* eslint-disable react-hooks/exhaustive-deps */
-    useEffect(() => {
-        execute(internal[kCommand], message);
-    }, [internal[kCommand]]);
-    /* eslint-enable */
-    useEffect(() => {
-        if (subscribe) {
-            return subscribe(message);
+export const cmdNone = {
+    cmdName: 'none',
+    execute() { }
+};
+export default function useElm(initialPair, update, subscriptions, enableLogging) {
+    const [internal, sendMessage] = useReducer((model, message) => {
+        const [newModel, command] = update(model, message);
+        if (enableLogging === true) {
+            console.group();
+            console.log(model);
+            console.log('+');
+            console.log(message);
+            console.log('---->');
+            console.log(newModel);
+            console.log(',');
+            console.log(command);
+            console.groupEnd();
         }
-    });
-    return [internal, message];
+        return toInternal([newModel, command]);
+    }, toInternal(initialPair));
+    useEffect(() => {
+        internal[kCommand].execute(sendMessage);
+    }, [internal[kCommand]]);
+    useEffect(() => {
+        const allCleanupFns = [];
+        for (const sub of subscriptions) {
+            const cleanupFn = sub.setup(sendMessage);
+            allCleanupFns.push(cleanupFn);
+        }
+        return () => {
+            for (const cleanupFn of allCleanupFns) {
+                cleanupFn();
+            }
+        };
+    }, []);
+    return [internal, sendMessage];
 }
+const kCommand = Symbol('kCommand');
 function toInternal(pair) {
     const [model, command] = pair;
     return {
--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,79 @@
 import React, { useEffect, useReducer } from 'react';
 
-export type BaseCommand = { type: string };
+export interface Cmd<M> {
+    cmdName: string;
+    execute(sendMessage: (m: M) => void): void;
+}
+
+export const cmdNone: Cmd<any> = { 
+    cmdName: 'none', 
+    execute() {} 
+};
+
+export interface Sub<M> {
+    subName: string;
+    setup(sendMessage: (m: M) => void): () => void;
+}
+
+export default function useElm<Model, Message>(
+    initialPair: [Model, Cmd<Message>],
+    update: (model: Model, message: Message) => [Model, Cmd<Message>],
+    subscriptions: Sub<Message>[],
+    enableLogging?: boolean
+): [Model, React.Dispatch<Message>] {
+    const [internal, sendMessage] = useReducer(
+        (model: InternalModel<Model, Message>, message: Message) => {
+            const [newModel, command] = update(model, message);
+
+            if (enableLogging === true) {
+                console.group();
+                console.log(model);
+                console.log('+');
+                console.log(message);
+                console.log('---->');
+                console.log(newModel);
+                console.log(',');
+                console.log(command);
+                console.groupEnd();
+            }
+
+            return toInternal([newModel, command]);
+        },
+
+        toInternal(initialPair)
+    );
+
+    useEffect(() => {
+        internal[kCommand].execute(sendMessage);
+    }, [internal[kCommand]]);
+
+    useEffect(() => {
+        const allCleanupFns: (() => void)[] = [];
+
+        for (const sub of subscriptions) {
+            const cleanupFn = sub.setup(sendMessage);
+            allCleanupFns.push(cleanupFn);
+        }
+
+        return () => {
+            for (const cleanupFn of allCleanupFns) {
+                cleanupFn();
+            }  
+        };
+    }, []);
+
+    return [internal, sendMessage];
+}
 
 const kCommand = Symbol('kCommand');
 
-export default function useElm<
-    Model, 
-    Message,
-    Command extends BaseCommand
->(
-    initialState: [Model, Command],
-    update: (model: Model, message: Message) => [Model, Command],
-    execute: (command: Command, message: (m: Message) => void) => void,
-    subscribe?: (message: (m: Message) => void) => () => void
-): [Model, React.Dispatch<Message>] {
-    const [internal, message] = useReducer(
-        (model: InternalModel<Model, Command>, message: Message) => {
-            return toInternal(update(model, message));
-        },
-
-        toInternal(initialState)
-    );
-
-    /* eslint-disable react-hooks/exhaustive-deps */
-    useEffect(() => {
-        execute(internal[kCommand], message);
-    }, [internal[kCommand]]);
-    /* eslint-enable */
-
-    useEffect(() => {
-        if (subscribe) {
-            return subscribe(message);
-        }
-    });
-
-    return [internal, message];
-}
-
-type InternalModel<Model, Command extends BaseCommand> = Model & {
-    [kCommand]: Command;
+type InternalModel<Model, Message> = Model & {
+    [kCommand]: Cmd<Message>;
 };
 
-function toInternal<Model, Command extends BaseCommand>(
-    pair: [Model, Command]
-): InternalModel<Model, Command> {
+function toInternal<Model, Message>(
+    pair: [Model, Cmd<Message>]
+): InternalModel<Model, Message> {
     const [model, command] = pair;
 
     return {
